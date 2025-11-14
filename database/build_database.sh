4c68d6064c465cd9b18dd4966d5b334ae80f3bb6
aleruiz <alejandro@posiwid.org>
2025-11-14 13:26:42 -0500

    Add database copying to networks

--- database/build_database.sh
+++ database/build_database.sh
@@ -1,3 +1,8 @@
+set -e
+
 rm -f output/papers.db
 alembic upgrade head
-python create_database.py 
+python create_database.py
+
+cd ../networks/input
+ln -s ../../database/output/papers.db papers.db
