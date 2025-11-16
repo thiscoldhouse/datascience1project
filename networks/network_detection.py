832e316f0b400cabc2f2779b2600e6a614476094
aleruiz <alejandro@posiwid.org>
2025-11-15 21:39:16 -0500

    First draft of second figure

--- networks/network_detection.py
+++ networks/network_detection.py
@@ -1,6 +1,9 @@
 import networkx as nx
+from nltk import bigrams as find_bigrams, word_tokenize, sent_tokenize
+import textwrap
 import matplotlib.pyplot as plt
-from sqlalchemy import create_engine
+import matplotlib.gridspec as gridspec
+from sqlalchemy import create_engine, desc, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import functions
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -8,6 +11,7 @@ import numpy as np
 import pandas as pd
 import os
 import sys
+
 sys.path.append(
     os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
@@ -22,11 +26,43 @@ engine = create_engine(
 )
 SessionFactory = sessionmaker(bind=engine)
 
-RESOLUTION=5
-MIN_GROUP_SIZE=10
+# -- move to config.py -- #
+RESOLUTION = 1
+TOP_N = 2
+n_terms = 10
+colors = [
+    '#DB9D47', '#8CC084', '#1B512D', '#433A3F', '#666A86', '#6A041D', '#28190E', '#090446', '#E86A92', '#009DDC', '#FFDDD2', '#A72608', '#63D2FF'
+]
+MIN_COMMUNITY_PAPERS = 10
+tables_dest = 'output/tables.md'
+graph_dest = 'output/graph.pdf'
 
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+stop.extend((
+    'elsevier', 'rights', 'reserved', 'mesh', 'taylor', 'francis', 'copyright', 'llc', 'bt', 'lftb', 'springer', 'ieee', 'information', 'misinformation'
+))
+stop = [w.lower() for w in stop]
+delete_title = 'Medical misinformation: vet the message!'
+
+# -- #
+
+def cleanup():
+    session = SessionFactory()
+    delete_papers = session.query(Paper).filter(
+        func.lower(Paper.title)==delete_title.lower()
+    ).all()
+    assert(len(delete_papers) in (22, 0))
+    [session.delete(p) for p in delete_papers]
+    session.commit()
 
 def label_papers_by_community(resolution=RESOLUTION):
+    """
+    Small world
+
+    https://sci-hub.ru/https://doi.org/10.1108/AJIM-09-2014-0116
+    """
+    cleanup()
     session = SessionFactory()
     G = nx.Graph()
     G.add_nodes_from([
@@ -40,8 +76,20 @@ def label_papers_by_community(resolution=RESOLUTION):
             for paper2 in author.papers[i+1:]:
                 G.add_edge(paper1.doi, paper2.doi)
                 
-    communities = nx.algorithms.community.louvain_communities(
-        G,
+    # rework network to penalize hubs
+    G_final = nx.Graph()
+    G_final.add_nodes_from([
+        p.doi for p in session.query(Paper).all()
+    ])
+    for u, v in G.edges():
+        G_final.add_edge(
+            u,
+            v,
+            weight = 1 / (G.degree[u] * G.degree[v])
+        )
+                           
+    communities = nx.algorithms.community.louvain_communities(   
+        G_final,
         resolution=resolution
     )
     
@@ -56,7 +104,7 @@ def label_papers_by_community(resolution=RESOLUTION):
             
     session.commit()
 
-def top_tfidf():
+def community_tfidf():
     session = SessionFactory()
     texts = (
         session.query(
@@ -71,11 +119,15 @@ def top_tfidf():
     ).all()
 
     texts = [
-        ' '.join(str(text[0]).split())
+        ' '.join(str(text[0]).split()).lower()
         for text in texts 
     ]
+    #texts = ['-'.join(b) for b in bigrams(texts)]
 
-    vectorizer = TfidfVectorizer()
+    vectorizer = TfidfVectorizer(
+        stop_words=stop,
+        ngram_range=(1,1),
+    )
     X = vectorizer.fit_transform(texts)
     terms = np.array(vectorizer.get_feature_names_out())
 
@@ -84,33 +136,58 @@ def top_tfidf():
     results = []
 
     for row in scores_array:
-        word_scores = [(terms[i], row[i]) for i in np.where(row > 0)[0]]
-        word_scores_sorted = sorted(word_scores, key=lambda x: x[1], reverse=True)
-        results.append(word_scores_sorted)
+        word_scores = [
+            (terms[i], row[i])
+            for i in np.where(row > 0)[0]
+        ]
+        word_scores_sorted = sorted(
+            word_scores,
+            key=lambda x: x[1],
+            reverse=True
+        )
+        results.append([
+            w[0] for w in word_scores_sorted
+        ])
 
     return results
 
 
-def make_graph(min_group_size=MIN_GROUP_SIZE):
-    label_papers_by_community()
+def make_graph(top_n=5, relabel=False, min_group_count=MIN_COMMUNITY_PAPERS):
+    if relabel is True:
+        label_papers_by_community()
+        
     session = SessionFactory()
-    communities = (
+
+    papers = pd.DataFrame(
         session.query(
             Paper.community,
-        ).
-        group_by(Paper.community).
-        having(
-            functions.count(Paper.doi) > min_group_size
-        )
-    ).all()
-    communities = [
-        c[0] for c in communities
-    ]
-
-    years = list(set([
-        p.year for p in session.query(Paper).all()
-    ]))
-
+            Paper.year
+        ).all()
+    )
+    papers.columns = ('community', 'year')
+    years = papers['year'].unique()
+    
+    communities = []
+    for year in years:
+        annual_comm = (
+            session.query(
+                Paper.community,
+                functions.count(Paper.doi),
+            ).
+            filter(Paper.year==int(year)).                
+            group_by(Paper.community).
+            having(
+                functions.count(Paper.doi) > min_group_count
+            ).
+            order_by(
+                desc(functions.count(Paper.doi)),
+            )
+        ).all()
+        communities.extend([
+            c[0] for c in annual_comm[:top_n]
+        ])
+        
+    communities = list(set(communities))
     data = {}
     papers = session.query(
         Paper.community,
@@ -118,29 +195,108 @@ def make_graph(min_group_size=MIN_GROUP_SIZE):
     ).filter(Paper.community.in_(communities)).all()
     df = pd.DataFrame(papers)
     df.columns = ('community', 'year')
+
+    community_to_tfidf = community_tfidf()
+    community_to_bigrams = list(community_bigrams())
+    
     plotdf = df.groupby('year').apply(
         lambda X: pd.Series({
             community: len(X[X.community==community])
             for community in communities
         })
-    ).plot(        
-        kind="area",
-        stacked=True
     )
+    for i, column in enumerate(plotdf.columns):
+        if column not in communities:
+            del plotdf[column]
+        else:
+            new_name =': '.join((
+                str(int(plotdf.columns[i])),
+                ', '.join([
+                        tfidf
+                        for tfidf in community_to_tfidf[column]
+                    ][:5])
+            ))
+            plotdf.rename(
+                columns={
+                    plotdf.columns[i]: new_name
+                },
+                inplace=True
+            )            
+            
+
+    plotdf.plot(        
+        kind="area",
+        stacked=True,
+        color=colors
+    )
+    
+    tfidf_table = pd.DataFrame({
+        'Community': [
+            c for c in communities
+        ],
+        f'Top {n_terms} TF-IDF': [
+            '\n'.join(community_to_tfidf[c][:n_terms])
+            for c in communities
+        ]
+    })
+    bigrams_table = pd.DataFrame({
+        'Community': [
+            c for c in communities
+        ],
+        f'Top {n_terms} Bigrams': [
+            '\n'.join(community_to_bigrams[c][:n_terms])
+            for c in communities
+        ]
+    })
+
+    
+    with open(tables_dest, 'w') as f:
+        f.write('\n\n\n\n'.join([
+            tfidf_table.to_markdown(index=False),
+            bigrams_table.to_markdown(index=False)
+        ]))
+        
+    plt.savefig(graph_dest)
     plt.show()
-    
-    # tfidf = top_tfidf()
-    # for paper in papers:
-    #     pass
+
         
-        
+def community_bigrams():
+    session = SessionFactory()
+    texts = (
+        session.query(
+            functions.concat(
+                Paper.title,
+                ' ',
+                Paper.abstract,
+            )
+        ).
+        group_by(Paper.community).
+        order_by(Paper.community)
+    ).all()
     
+    for text in texts:
+        text = text[0]
+        result = find_bigrams(word_tokenize(text.lower()))
+        bigrams = []
+        for bg in result:
+            if any([
+                word in stop for word in bg
+            ]) or any([
+                not word.isalpha() for word in bg                
+            ]):
+                continue            
+            bigrams.append(' '.join(bg))
+            
+        bigrams = sorted(
+            ((s, bigrams.count(s)) for s in set(bigrams)), 
+            key=lambda x: -x[1]
+        )
+        yield [b[0] for b in bigrams]
 
 
+        
 if __name__ == '__main__':
-    #label_papers_by_community()
-    #results = top_tfidf()
-    make_graph(15)
+    make_graph(top_n=TOP_N, relabel=False)
             
             
         
