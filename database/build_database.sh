6954f926c5d1444a34889121506add9618eabef2
aleruiz <alejandro@posiwid.org>
2025-11-25 20:39:10 -0500

    Add new folder inputs to symlinking in build_database.sh

--- database/build_database.sh
+++ database/build_database.sh
@@ -2,12 +2,18 @@ set -e
 
 mkdir -p output
 mkdir -p ../networks/input
+mkdir -p ../citations/input
 
 rm -f output/papers.db
 alembic upgrade head
 python create_database.py
 
 cd ../networks/input
+rm -f papers.db
+ln -s ../../database/output/papers.db papers.db
+cd -
+
+cd ../citations/input
 rm -f papers.db
 ln -s ../../database/output/papers.db papers.db
 cd -
