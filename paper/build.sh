dc0b713d9f412622e48482db6c0725f8ecb03e13
aleruiz <alejandro@posiwid.org>
2026-01-01 04:55:49 -0500

    Fixed enormous git disaster (and a few minor typos in main.tex).

--- paper/build.sh
+++ paper/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-rm -f main.aux main.bbl main.blg main.run.xml main.toc main.lof main.lot main.out
+rm -f main.aux main.bbl main.blg main.run.xml main.toc main.lof main.lot main.out main.log
 
 pdflatex main
 bibtex main
