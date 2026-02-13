3f900962073df7013a0cccb8166731adc44f7805
aleruiz <alejandro@posiwid.org>
2026-02-13 15:31:53 -0500

    Remove symlinks to link with overleaf

--- paper/build.sh
+++ paper/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+rm -rf citations-img misc-img network-img term_frequency-img
+
+cp -r ../src/citations/output citations-img/
+cp -r ../src/misc/output misc-img/
+cp -r ../src/networks/output network-img/
+cp -r ../src/term_frequency/output term_frequency-img/
+
 rm -f main.aux main.bbl main.blg main.run.xml main.toc main.lof main.lot main.out main.log
 
 pdflatex main
